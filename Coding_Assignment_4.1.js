ce05dfd39379022d7dced69ffe866b4050e96e77
Mohammed Nabeel P <111896737+mdnabeelMCA23@users.noreply.github.com>
2023-10-12 20:49:03 +0530

    Walk_boston

---
+++ Coding_Assignment_4.1.js
@@ -0,0 +1,24 @@
+import {boston} from"./boston.js"
+var newBoston=boston.data
+var ele="";
+for(let i=0;i<newBoston.length;i++){
+    for(let j=i+1;j<newBoston.length;j++){
+        if(newBoston[i][11]<newBoston[j][11]){
+            let temp=newBoston[i];
+            newBoston[i]=newBoston[j]
+            newBoston[j]=temp;
+        }
+    }
+}
+
+let output=newBoston.slice(0,5);
+for(let i=0;i<output.length;i++){
+ele=ele+"<h1>"+output[i][8]+"</h1>"+"<h1>"+output[i][11]+"</h1>"
+console.log(output[i][8],output[i][11])
+}
+document.getElementById("app").innerHTML=ele
+// for(let i=0;i<newboston.length;i++){
+//     ele=ele+"<h1>"+newboston[i][8]+"</h2>"+"<h1>"+newboston[i][11]+"</h2>"
+// console.log(newboston[i][8],newboston[i][11])
+// }
+// document.getElementById("app").innerHTML=ele;
